e3a0d173aca28aeb5fd359b9aaaefe749b34fcf0
lyx <doo8opnewman@outlook.com>
2024-07-30 20:07:11 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+﻿//欧拉筛法，素数筛
+#include<iostream>
+using namespace std;
+int prime[50];
+int main()
+{
+	int size = 1;
+	prime[0] = 2;
+	for (int i = 2; i <= 100; i++)
+	{
+		cout << "当前数：" << i<<endl;
+		cout << "素数有：";
+		for (int k = 0; k < size; k++)
+		{
+			cout << prime[k] << " ";
+		}
+		cout << endl;
+		for (int j = 0; j < size; j++)
+		{
+			cout << prime[j] * i << " ";
+			if (i % prime[j] == 0)
+			{
+				break;
+			}
+			if (j == size - 1)
+			{
+				prime[size++] = i;
+			}
+		}
+		cout << endl;
+	}
+
+
+	cout << "素数有：" << endl;;
+	cout << size << "个" << endl << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << prime[i] << " ";
+	}
+}
---
+++ summer24.7.30/summer24.7.30.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+int extend_gcd(int a, int b, int& x, int& y)
+{
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+	else
+	{
+		int temp = extend_gcd(b, a % b, x, y);
+		int t = y;
+		y = x - (a / b) * y;
+		x = t;
+		return temp;
+	}
+}
+int main()
+{
+	int a, b;
+	int x, y;
+	int c;
+	cin >> a >> b;
+	int g = extend_gcd(a, b, x, y);
+	cout << "最大公约数是：" << g << endl;
+	cout << "请输入c的值：";
+	cin >> c;
+	if (c % g == 0)
+		cout << "有解";
+	cout << "x:" << x << endl << "y:" << y << endl;
+	x += b / g;
+	y -= a / g;
+	cout << "最小x正整数解(=gcd(a,b),与c无关)" << endl << "x:" << x << endl << "y:" << y << endl;
+	if (c % g != 0)
+		cout << "同余式无解";
+	else
+	{
+		cout << "同余式" << endl;
+		x *= c / g;
+		y *= c / g;
+		for (int i = 0; i < g; i++)
+		{
+			cout << (x + b / g * i) % b << endl << (y + b / g * i) % b;
+		}
+	}
+}
